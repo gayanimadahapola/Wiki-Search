a50e2ba2e724d67cfd1ba352061d21168410840d
gayanimadahapola <gayanimadahapola@users.github.com>
2020-02-05 00:40:46 +0530

    Initial Commit

---
+++ Wiki Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wiki Search
+//
+//  Created by Gayani Madubhashini on 2/5/20.
+//  Copyright © 2020 Gayani Madubhashini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
